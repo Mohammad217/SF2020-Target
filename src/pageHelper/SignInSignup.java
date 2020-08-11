32825eb0df94df638393b229d30d4b656df49992
alaud <alaud@Mohammad-uddin>
2020-08-11 18:47:32 -0400

    2nd

--- src/pageHelper/SignInSignup.java
+++ src/pageHelper/SignInSignup.java
@@ -4,19 +4,19 @@ public class SignInSignup {
 
 	// all the locators --- xpath, id, css, name, custom xapth ect
 	
-	public String signInLinkLoc 		= "//*[@id='current_account']"; // xpath
-	//*[@id="b_tt_holder_2"]/div
+	public String signInLinkLoc 		= "//*[@id=\'account\']/span[4]"; // xpath
+	public String signInClickLoc        ="//*[@id=\'accountNav-signIn\']/a/div";
 	public String emailLoc 				= "#username"; // css selector
-	public String nextButtonLoc			= "//*[@id='root']/div/div[2]/div/div[1]/div[2]/div/div/div/form/div[3]/button";
-	public String passwordLoc			= "//*[@id='password']";
-	public String signInButtonLoc		= "//*[@id='root']/div/div[2]/div/div[1]/div[2]/div/div/div/form/button";
-	public String myActualFullNameLoc	= "//*[@id='profile-menu-trigger--content']";
+	//public String nextButtonLoc			= "//*[@id='root']/div/div[2]/div/div[1]/div[2]/div/div/div/form/div[3]/button";
+	public String passwordLoc			= "//*[@id=\'password\']";
+	public String signInButtonLoc		= "//*[@id=\'login\']";
+	//public String myActualFullNameLoc	= "//*[@id='profile-menu-trigger--content']";
 	
-	public String exectedEmailPassErrorMsg = "//*[@id='password-error']";
+	//public String exectedEmailPassErrorMsg = "//*[@id='password-error']";
 
 	// all the values -- test data, name, email, password
-	public String emailValue 			= "taltekc@gmail.com";
-	public String passValue				= "Te$t1234";
+	public String emailValue 			= "alauddinripon@gmail.com";
+	public String passValue				= "Samara25";
 	public String invalidPassValue		= "Te$t1234w";
 	
 }
